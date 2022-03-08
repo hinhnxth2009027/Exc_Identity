ee9812690b8fa56064712a9510667a52b462eb6f
hinhnxth2009027 <nguyenhjnh2002@gmail.com>
2022-03-08 20:55:19 +0700

    init project

---
+++ Asignnment/Data/MyIdentityDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Asignnment.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Asignnment.Data
+{
+    public class MyIdentityDbContext : IdentityDbContext<User>
+    {
+        public MyIdentityDbContext() : base("connectionStrings")
+        {
+        }
+    }
+}
